32669997126bec163dcaa062c9a87fa93b84b951
Subham Dey <sarmisthaad@gmail.com>
2026-07-08 21:04:47 +0530

    ChatAi Added

--- src/pages/ProblemPage.jsx
+++ src/pages/ProblemPage.jsx
@@ -4,6 +4,7 @@ import Editor from '@monaco-editor/react';
 import { useParams } from 'react-router';
 import axiosClient from "../utils/axiosClient"
 import SubmissionHistory from "../components/SubmissionHistory"
+import ChatAi from '../components/ChatAi';
 
 const langMap = {
         cpp: 'C++',
@@ -193,6 +194,13 @@ const ProblemPage = () => {
           >
             Submissions
           </button>
+
+           <button 
+            className={`tab ${activeLeftTab === 'chatAI' ? 'tab-active' : ''}`}
+            onClick={() => setActiveLeftTab('chatAI')}
+          >
+            ChatAI
+          </button>
         </div>
 
         {/* Left Content */}
@@ -270,6 +278,14 @@ const ProblemPage = () => {
                   </div>
                 </div>
               )}
+              {activeLeftTab === 'chatAI' && (
+                              <div className="prose max-w-none">
+                                <h2 className="text-xl font-bold mb-4">CHAT with AI</h2>
+                                <div className="whitespace-pre-wrap text-sm leading-relaxed">
+                                  <ChatAi problem={problem}></ChatAi>
+                                </div>
+                              </div>
+                            )}
             </>
           )}
         </div>
